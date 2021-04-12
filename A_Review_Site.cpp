aac2912eae649fce0a282e83b475d7b2dfce866e
Sumit Lovanshi <sumitlovanshi2112@gmail.com>
2021-04-13 00:54:32 +0530

    13-4-21

---
+++ A_Review_Site.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int arr[n];
+        for(int i=0;i<n;i++)
+        {
+            cin>>arr[i];
+        }
+        int a=0;
+        for(int i=0;i<n;i++){
+            if(arr[i] == 1){
+                a++;
+            }
+            if(arr[i]==3){
+                a++;
+            }
+        }
+        cout<<a<<"\n";
+    }
+    return 0;
+}
